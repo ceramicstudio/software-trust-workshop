0e731f79695e93a698b07a71f50b704a93783282
Mark Krasner <markkrasner@Marks-MacBook.localdomain>
2024-03-18 10:16:11 -0400

    Adding queries and updating schemas

--- src/pages/query.tsx
+++ src/pages/query.tsx
@@ -8,83 +8,47 @@ import { useAccount } from "wagmi";
 import { GraphiQL } from "graphiql";
 import { definition } from "../__generated__/definition.js";
 import { ComposeClient } from "@composedb/client";
+import { useComposeDB } from "../fragments";
 import "graphiql/graphiql.min.css";
 
 const Home: NextPage = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const { address, isDisconnected } = useAccount();
-
+  const { compose } = useComposeDB();
 
   const verifiableCredentialQuery = 
 `
-query VerifiableCredentialsAll {
-  verifiableCredentialIndex(first: 10) {
-    edges {
-      node {
-        controller {
-          id
+mutation CreateAccountTrustSignal {
+  setAccountTrustSignal(input: {
+    content: {
+      recipient: "did:pkh:eip155:1:0xc362c16a0dcbea78fb03a8f97f56deea905617bb"
+      issuanceDate: "2021-10-01T00:00:00Z"
+      trustWorthiness: [
+        {
+          scope: "Honesty"
+          level: 0.5
+          reason: ["Alumnus"]
         }
-        issuer {
-          id
-        }
-        context
-        type
-        credentialSchema {
-          id
-          type
-        }
-        issuanceDate
-        ... on AccountTrustCredential712 {
-          credentialSubject {
-            id {
-              id
-            }
-            trustworthiness {
-              type
-              level
-              scope
-              reason
-            }
-          }
-          proof {
-            verificationMethod
-            created
-            proofPurpose
-            type
-            proofValue
-            eip712 {
-              domain {
-                chainId
-                name
-                version
-              }
-              types {
-                EIP712Domain {
-                  name
-                  type
-                }
-                CredentialSchema {
-                  name
-                  type
-                }
-                CredentialSubject {
-                  name
-                  type
-                }
-                Proof {
-                  name
-                  type
-                }
-                VerifiableCredential {
-                  name
-                  type
-                }
-              }
-              primaryType
-            }
-          }
-        }
+      ]
+      proof: "eyJwYXlsb2FkIjoiZXlKQmMzQmxZM1JoSWpvaUlpd2lRMmhoY20xV1pYSnpaU0k2SWlJc0lrUmxVR2x1UkdGNUlqb2lhMnA2YkRaclkzbHROM2M0ZVRkMlpUaDVkSE5uY1dZemVuaHNhakJzT1hWaFlUUmpOR0V4TldObE9IQnJlWEJ0WlRSdVkzaHZkM0YzY1drNWJHd3lJaXdpUkdWVFkybEVZWGtpT2lKcmFucHNObXRqZVcwM2R6aDVOalpqTW5FNGJHYzVPSGgyTjJKM2VYcHROMmhtYlRKMmNXSnVkbVJ2TWpkdk9HZHdkV3RyTVRKaU1tWjZjRzlyZEdvaUxDSkdiSFZsYm1ObFFtOXZkR2dpT2lJaUxDSlBjR1Z1UkdGMFlVUmhlU0k2SWlJc0lsQnliMjltVDJaRVlYUmhJam9pYTJwNmJEWnJZM2x0TjNjNGVUaHVialpsZUc1MGJqY3hPVEJtWTJ4ck4zRXlOR1Y1YVROdmMybHRjM1Z2ZUdSelp6VXdNRGcyTURkc00yaDJhVFI0SWl3aVZHRnNaVzUwUkdGdlNHRmphMlZ5U0c5MWMyVWlPaUlpTENKbGRtVnVkQ0k2SWxSb2NtVmxRbUZrWjJWeklpd2ljbVZqYVhCcFpXNTBJam9pTUhneU5HVmlNRFE1TXpFMk5qWmlPV0UxTXpJNVpEUTFZakZqWldKa1lqQTNOV1prTXpobE9XSTRJaXdpZEdsdFpYTjBZVzF3SWpvaU1qQXlOQzB3TXkwd05sUXhPVG95TlRvd09TNDNNVGhhSW4wIiwic2lnbmF0dXJlcyI6W3sicHJvdGVjdGVkIjoiZXlKaGJHY2lPaUpGWkVSVFFTSXNJbXRwWkNJNkltUnBaRHByWlhrNmVqWk5hMmhGVWpVeE9ERnRkRGxRUWtOeWJsWjJURGxCWTJSWGVYcFRlbW8wVUV4blIxWkxVMFpxU2podllrMU9JM28yVFd0b1JWSTFNVGd4YlhRNVVFSkRjbTVXZGt3NVFXTmtWM2w2VTNwcU5GQk1aMGRXUzFOR2FrbzRiMkpOVGlKOSIsInNpZ25hdHVyZSI6IlB2WGdWUFdqVktiRThjdnVFRFBPaG5hb1lBamh3am5JRVZGZWZyS2JxYndHVU9aSktFOEZBczJZeWg4RUxXVC1mZkpUc1lvTzBxNi1oaDgtMW9obERnIn1dfQ=="
+    }
+  })
+  {
+    document {
+      id
+      issuer{
+        id
       }
+      recipient {
+        id
+      }
+      issuanceDate
+      trustWorthiness {
+        level
+        scope
+        reason
+      }
+      proof
     }
   }
 }
@@ -92,60 +56,39 @@ query VerifiableCredentialsAll {
 
 const verifiableCredentialQuery1 = 
 `
-query AccountTrustEIP712 {
-  accountTrustCredential712Index(last: 1) {
-    edges {
-      node {
-        credentialSubject {
-          id {
-            id
-          }
-          trustworthiness {
-            type
-            level
-            scope
-            reason
-          }
+mutation CreateSecurityAudit {
+  setSecurityAudit(input: {
+    content: {
+      subjectId: "snap://CLwZocaUEbDErtQAsybaudZDJq65a8AwlEFgkGUpmAQ="
+      issuanceDate: "2021-10-01T00:00:00Z"
+      securityStatus: true
+      securityFindings: [
+        {
+          criticality: 0.5
+          type: "Data leak"
+          description: "API can communicate data to a centralized server"
+          lang: "en"
         }
-        issuanceDate
-        proof {
-          verificationMethod
-          created
-          proofPurpose
-          type
-          proofValue
-          eip712 {
-            domain {
-              chainId
-              name
-              version
-            }
-            types {
-              EIP712Domain {
-                name
-                type
-              }
-              CredentialSchema {
-                name
-                type
-              }
-              CredentialSubject {
-                name
-                type
-              }
-              Proof {
-                name
-                type
-              }
-              VerifiableCredential {
-                name
-                type
-              }
-            }
-            primaryType
-          }
-        }
+      ]
+      proof: "eyJwYXlsb2FkIjoiZXlKQmMzQmxZM1JoSWpvaUlpd2lRMmhoY20xV1pYSnpaU0k2SWlJc0lrUmxVR2x1UkdGNUlqb2lhMnA2YkRaclkzbHROM2M0ZVRkMlpUaDVkSE5uY1dZemVuaHNhakJzT1hWaFlUUmpOR0V4TldObE9IQnJlWEJ0WlRSdVkzaHZkM0YzY1drNWJHd3lJaXdpUkdWVFkybEVZWGtpT2lKcmFucHNObXRqZVcwM2R6aDVOalpqTW5FNGJHYzVPSGgyTjJKM2VYcHROMmhtYlRKMmNXSnVkbVJ2TWpkdk9HZHdkV3RyTVRKaU1tWjZjRzlyZEdvaUxDSkdiSFZsYm1ObFFtOXZkR2dpT2lJaUxDSlBjR1Z1UkdGMFlVUmhlU0k2SWlJc0lsQnliMjltVDJaRVlYUmhJam9pYTJwNmJEWnJZM2x0TjNjNGVUaHVialpsZUc1MGJqY3hPVEJtWTJ4ck4zRXlOR1Y1YVROdmMybHRjM1Z2ZUdSelp6VXdNRGcyTURkc00yaDJhVFI0SWl3aVZHRnNaVzUwUkdGdlNHRmphMlZ5U0c5MWMyVWlPaUlpTENKbGRtVnVkQ0k2SWxSb2NtVmxRbUZrWjJWeklpd2ljbVZqYVhCcFpXNTBJam9pTUhneU5HVmlNRFE1TXpFMk5qWmlPV0UxTXpJNVpEUTFZakZqWldKa1lqQTNOV1prTXpobE9XSTRJaXdpZEdsdFpYTjBZVzF3SWpvaU1qQXlOQzB3TXkwd05sUXhPVG95TlRvd09TNDNNVGhhSW4wIiwic2lnbmF0dXJlcyI6W3sicHJvdGVjdGVkIjoiZXlKaGJHY2lPaUpGWkVSVFFTSXNJbXRwWkNJNkltUnBaRHByWlhrNmVqWk5hMmhGVWpVeE9ERnRkRGxRUWtOeWJsWjJURGxCWTJSWGVYcFRlbW8wVUV4blIxWkxVMFpxU2podllrMU9JM28yVFd0b1JWSTFNVGd4YlhRNVVFSkRjbTVXZGt3NVFXTmtWM2w2VTNwcU5GQk1aMGRXUzFOR2FrbzRiMkpOVGlKOSIsInNpZ25hdHVyZSI6IlB2WGdWUFdqVktiRThjdnVFRFBPaG5hb1lBamh3am5JRVZGZWZyS2JxYndHVU9aSktFOEZBczJZeWg4RUxXVC1mZkpUc1lvTzBxNi1oaDgtMW9obERnIn1dfQ=="
+    }
+  })
+  {
+    document {
+      id
+      issuer{
+        id
       }
+      subjectId
+      issuanceDate
+      securityStatus
+      securityFindings {
+        criticality
+        type
+        description
+        lang
+      }
+      proof
     }
   }
 }
@@ -159,10 +102,7 @@ query AccountTrustEIP712 {
   }
 
   const fetcher = async (graphQLParams: Record<string, any>) => {
-    const composeClient = new ComposeClient({
-      ceramic: "http://localhost:7007",
-      definition: definition as any,
-    });
+    const composeClient = compose as ComposeClient;
 
     const data = await composeClient.executeQuery(`${graphQLParams.query}`);
     console.log(data);
